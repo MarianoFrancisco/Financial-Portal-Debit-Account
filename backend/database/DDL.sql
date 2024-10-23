2b7ceda2fc3b065a57c2dc56dfbd04721c3cc461
Mariano Camposeco <mariano1941@outlook.es>
2024-10-23 00:38:29 -0600

    feat: change register date

--- backend/database/DDL.sql
+++ backend/database/DDL.sql
@@ -10,7 +10,22 @@ CREATE TABLE IF NOT EXISTS user (
     pin VARCHAR(255) NOT NULL,
     phone VARCHAR(15),
     genre ENUM('Male', 'Female', 'Other') DEFAULT 'Other',
-    registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    registration_date BIGINT NOT NULL DEFAULT UNIX_TIMESTAMP(),
+    active TINYINT(1) DEFAULT 1,
+    notifyme TINYINT(1) DEFAULT 0
+) ENGINE=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(15) NOT NULL UNIQUE,
+    name VARCHAR(65) NOT NULL,
+    lastname VARCHAR(65),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    pin VARCHAR(255) NOT NULL,
+    phone VARCHAR(15),
+    genre ENUM('Male', 'Female', 'Other') DEFAULT 'Other',
+    registration_date BIGINT NOT NULL DEFAULT UNIX_TIMESTAMP(),
     active TINYINT(1) DEFAULT 1,
     notifyme TINYINT(1) DEFAULT 0
 ) ENGINE=InnoDB;
