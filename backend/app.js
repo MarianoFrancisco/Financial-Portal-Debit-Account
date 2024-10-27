9e64f31e2ea1de6f2e6c391df84af39aae209d11
Mariano Camposeco <mariano1941@outlook.es>
2024-10-27 12:34:40 -0600

    feat: new routes in api

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import authenticatorRouter from './app/routes/authenticator.routes.js';
+import tokenLinkedApiRouter from './app/routes/token-linked-api.routes.js';
+import bankAccountRouter from './app/routes/bank-account.routes.js';
 
 const app = express();               
 
@@ -15,7 +17,11 @@ app.use(cors());
 
 const api = '/api';
 const authenticator = `${api}/authenticator`;
+const tokenLinkedApi = `${api}/token-linked-api`;
+const bankAccount = `${api}/bank-account`;
 
 app.use(authenticator, authenticatorRouter);
+app.use(tokenLinkedApi, tokenLinkedApiRouter);
+app.use(bankAccount, bankAccountRouter);
 
 export default app;
