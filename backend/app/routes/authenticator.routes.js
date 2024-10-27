858e17158fe4a47bdde529226eaa40090f1b0625
Mariano Camposeco <mariano1941@outlook.es>
2024-10-27 12:33:29 -0600

    feat: new routes

--- backend/app/routes/authenticator.routes.js
+++ backend/app/routes/authenticator.routes.js
@@ -3,10 +3,15 @@
 * Mariano Camposeco {@literal (mariano1941@outlook.es)}
 */
 import express from 'express';
-const router = express.Router();
-import { register, login } from '../controllers/authenticator-controller.js';
+import {
+    register,
+    login
+} from '../controllers/authenticator-controller.js';
+import { administratorProtect } from '../middleware/role-protect-middleware.js';
 
-router.post('/register', register);
+const router = express.Router();
+
+router.post('/register', administratorProtect, register);
 router.post('/login', login);
 
 export default router;
