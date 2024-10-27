d607a4d457039a497c3a5dccb5d1106ea083c26c
Mariano Camposeco <mariano1941@outlook.es>
2024-10-27 12:28:08 -0600

    feat: add new tables and dml

--- backend/database/DDL.sql
+++ backend/database/DDL.sql
@@ -1,22 +1,29 @@
 CREATE DATABASE IF NOT EXISTS financial_portal_debit_account;
 USE financial_portal_debit_account;
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS currencies (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(15) NOT NULL UNIQUE,
-    name VARCHAR(65) NOT NULL,
-    lastname VARCHAR(65),
-    email VARCHAR(100) NOT NULL UNIQUE,
-    pin VARCHAR(255) NOT NULL,
-    phone VARCHAR(15),
-    genre ENUM('Male', 'Female', 'Other') DEFAULT 'Other',
-    registration_date BIGINT NOT NULL DEFAULT UNIX_TIMESTAMP(),
-    active TINYINT(1) DEFAULT 1,
-    notifyme TINYINT(1) DEFAULT 0
+    code VARCHAR(3) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL UNIQUE
 ) ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS account_tiers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tier_name VARCHAR(50) NOT NULL UNIQUE,
+    opening_fee DECIMAL(10, 2) NOT NULL
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS tokens_linked_api (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    token VARCHAR(1024) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(15) NOT NULL UNIQUE,
     name VARCHAR(65) NOT NULL,
@@ -25,7 +32,62 @@ CREATE TABLE IF NOT EXISTS user (
     pin VARCHAR(255) NOT NULL,
     phone VARCHAR(15),
     genre ENUM('Male', 'Female', 'Other') DEFAULT 'Other',
-    registration_date BIGINT NOT NULL DEFAULT UNIX_TIMESTAMP(),
+    registration_date BIGINT NOT NULL,
     active TINYINT(1) DEFAULT 1,
-    notifyme TINYINT(1) DEFAULT 0
+    notifyme TINYINT(1) DEFAULT 0,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS bank_accounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    account_name VARCHAR(255) NOT NULL UNIQUE,
+    account_number BIGINT NOT NULL UNIQUE,
+    user_id INT NOT NULL,
+    account_tier_id INT NOT NULL,
+    currency_id INT NOT NULL,
+    balance DECIMAL(18, 2) DEFAULT 0.00,
+    creation_date BIGINT NOT NULL,
+    is_delete INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (account_tier_id) REFERENCES account_tiers(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (currency_id) REFERENCES currencies(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS user_api_links (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    external_user_id INT NOT NULL,
+    account_id INT NOT NULL,
+    origin_api VARCHAR(100) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (account_id) REFERENCES bank_accounts(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    bank_account_id INT NOT NULL,
+    transaction_type ENUM('Income', 'Expense') NOT NULL,
+    amount DECIMAL(18, 2) NOT NULL,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (bank_account_id) REFERENCES bank_accounts(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    INDEX(bank_account_id, transaction_date)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS exchange_rates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    origin_currency_id INT NOT NULL,
+    destination_currency_id INT NOT NULL,
+    rate DECIMAL(10, 4) NOT NULL,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (origin_currency_id) REFERENCES currencies(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (destination_currency_id) REFERENCES currencies(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS account_closures (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    bank_account_id INT NOT NULL,
+    closure_reason VARCHAR(255),
+    closure_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (bank_account_id) REFERENCES bank_accounts(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
--- backend/database/DML.sql
+++ backend/database/DML.sql
@@ -1,1 +1,30 @@
 USE financial_portal_debit_account;
+INSERT INTO currencies (id, code, name)
+VALUES 
+    (1, 'GTQ', 'Quetzal'),
+    (2, 'USD', 'US Dollar'),
+    (3, 'EUR', 'Euro')
+ON DUPLICATE KEY UPDATE
+    name = VALUES(name);
+
+INSERT INTO account_tiers (id, tier_name, opening_fee)
+VALUES 
+    (1, 'basic', 200.00),
+    (2, 'premium', 50.00),
+    (3, 'plus', 50.00)
+ON DUPLICATE KEY UPDATE
+    opening_fee = VALUES(opening_fee);
+
+INSERT INTO roles (id, role_name)
+VALUES
+    (1, 'Administrator'),
+    (2, 'User');
+
+INSERT INTO users (id, username, name, lastname, email, pin, phone, genre, registration_date, active, notifyme, role_id) VALUES 
+(1, 'mariano', 'Mariano', 'Camposeco', 'mariano1941@outlook.es', 'a3db58d6c14880dacffd3275f5f897ef:bc5512706f3fae451eec82057ad290d4', '+50231840649', 'Male', UNIX_TIMESTAMP(), 1, 1, 2);
+
+INSERT INTO bank_accounts (id, account_name, account_number, user_id, account_tier_id, currency_id, balance, creation_date, is_delete)
+VALUES 
+    (1, 'mariano.basic@kizunamaya.com', 1234567890, 1, 1, 1, 5000.00, UNIX_TIMESTAMP(), 0),
+    (2, 'mariano.premium@kizunamaya.com', 9876543210, 1, 2, 2, 15000.00, UNIX_TIMESTAMP(), 0),
+    (3, 'mariano.plus@kizunamaya.com', 1122334455, 1, 3, 1, 2500.50, UNIX_TIMESTAMP(), 0);
