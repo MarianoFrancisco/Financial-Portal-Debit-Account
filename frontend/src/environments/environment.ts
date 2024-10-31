d2b699530b620b40f153987f68da5f854e38a80a
Mariano Camposeco <mariano1941@outlook.es>
2024-10-31 15:51:24 -0600

    feat: add prod environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    API_URL:    'http://localhost:5000/api',
+    //API_URL:    'http://localhost:5000/api',
+    API_URL:    'https://financial-portal-debit-account.onrender.com/api',
     production: false
 }
